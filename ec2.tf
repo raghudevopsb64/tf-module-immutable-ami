631438db9ed4d95e110535357305b4dada934162
Raghu K <raghuk.vit@gmail.com>
2022-07-06 07:45:18 +0530

    Update roboshop-infra/all-modules/tf-module-immutable-ami/

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ami" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.small"
   vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
 
   tags = {
     Name = "${var.COMPONENT}-ami"
